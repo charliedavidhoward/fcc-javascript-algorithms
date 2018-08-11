5131320e8c044483d76fc1f85a416884748102c5
charliedavidhoward <41549599+charliedavidhoward@users.noreply.github.com>
2018-08-11 21:24:08 +0100

    Update spinal-tap-case.js

--- intermediate/spinal-tap-case.js
+++ intermediate/spinal-tap-case.js
@@ -1,9 +1,22 @@
 // Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
 function spinalCase(str) {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
-  return str;
+
+  // split at whitespace, underscore, or before a capital letter
+return str.split(/\s|_|(?=[A-Z])/)
+          // join the string back up with a hyphen
+          .join("-")
+          // set all characters to lower case
+          .toLowerCase();
+
+// function returns "this-is-spinal-tap"
+
+/* Example returns:
+"all-the-small-things" for "AllThe-small Things"
+"teletubbies-say-eh-oh" for "Teletubbies say Eh-oh"
+"the-andy-griffith-show" for "The_Andy_Griffith_Show"
+*/
+
 }
 
 spinalCase('This Is Spinal Tap');
