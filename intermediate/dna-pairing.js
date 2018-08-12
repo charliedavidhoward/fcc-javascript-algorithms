3a385282eeb146be2e105ce79d9aab9a8f6601c1
charliedavidhoward <41549599+charliedavidhoward@users.noreply.github.com>
2018-08-12 08:00:31 +0100

    Update dna-pairing.js

--- intermediate/dna-pairing.js
+++ intermediate/dna-pairing.js
@@ -5,7 +5,31 @@
 // The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 
 function pairElement(str) {
-  return str;
+
+  // create an empty array to store the result
+  var newArray = [];
+
+  // loop through the input string and use switch for each of the four cases
+  for (var i = 0; i < str.length; i++) {
+    switch(str[i]) {
+      case "G":
+        newArray.push(["G", "C"]);
+        break;
+      case "C":
+        newArray.push(["C", "G"]);
+        break;
+      case "A":
+        newArray.push(["A", "T"]);
+        break;
+      case "T":
+        newArray.push(["T", "A"]);
+        break;
+      default:
+        break;
+    }
+  }
+  
+  return newArray;
 }
 
 pairElement("GCG");
