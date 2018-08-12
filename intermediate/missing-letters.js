8f67e3650d8aac6eaafc3eea61c0420e24067cd8
charliedavidhoward <41549599+charliedavidhoward@users.noreply.github.com>
2018-08-12 09:41:48 +0100

    Update missing-letters.js

--- intermediate/missing-letters.js
+++ intermediate/missing-letters.js
@@ -2,7 +2,34 @@
 //If all letters are present in the range, return undefined.
 
 function fearNotLetter(str) {
-  return str;
-}
+
+  // set a variable to the ASCII value of the first character in the string
+  var value = str.charCodeAt(0);
+  
+  // set a variable to store the missing character
+  // initialised to undefined 
+  var missing;
+
+  // split the string into an array of characters
+  str.split("")
+  
+      // use map to iterate through each character
+     .map(function(currentValue) {
+    
+       // if the current character matches the value, increase the value by one
+       // (value set to the next ASCII value)
+       if (currentValue.charCodeAt() == value) {
+         value++;
+         
+       // otherwise, we have found the missing value
+       // set missing to the value using fromCharCode to pull the character from the ASCII value
+       } else {
+         missing = String.fromCharCode(value);
+       }
+    });
+
+  return missing;
+
+  }
 
 fearNotLetter("abce");
