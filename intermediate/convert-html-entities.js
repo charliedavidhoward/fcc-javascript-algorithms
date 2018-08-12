c1e65771f04f54d374573c5fe3f09bad76ccc847
charliedavidhoward <41549599+charliedavidhoward@users.noreply.github.com>
2018-08-12 16:15:43 +0100

    Update convert-html-entities.js

--- intermediate/convert-html-entities.js
+++ intermediate/convert-html-entities.js
@@ -1,8 +1,24 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
 function convertHTML(str) {
-  // &colon;&rpar;
+ 
+  // create an object for the characters in the spec matched to their corresponding HTML entities
+  var entities = 
+  {"&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  "\"": "&quot;",
+  "\'": "&apos;"};
+
+  // use a regular expression in .replace and call a function to
+  // replace the value with the pair frm the entities object
+  str = str.replace(/[&<>\"\']/g, function(input) {
+    return entities[input];
+  });
+  
   return str;
 }
 
 convertHTML("Dolce & Gabbana");
+
+// returns "Dolce &amp; Gabbana"
