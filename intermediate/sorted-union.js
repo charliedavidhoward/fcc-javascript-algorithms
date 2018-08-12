d9c7a432e322ee29d502dd7c2db414c1ef71a6cc
charliedavidhoward <41549599+charliedavidhoward@users.noreply.github.com>
2018-08-12 11:16:50 +0100

    Update sorted-union.js

--- intermediate/sorted-union.js
+++ intermediate/sorted-union.js
@@ -3,7 +3,22 @@
 // The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
 
 function uniteUnique(arr) {
-  return arr;
+  
+  var result = [];
+  
+  // the number of arguments are unknown
+  // use a for loop to iterate over all the arguments and push them into result
+  for (var i = 0; i < arguments.length; i++) {
+    result = result.concat(arguments[i])
+  };
+
+  // filter the result array by checking the index of the element against the current index
+  // if the indexof is different, the element has already been encountered, so return false and remove the element
+  result = result.filter(function(element, index) {
+    return (result.indexOf(element) === index) }
+  );
+
+  return result;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
