a51aed6f31d91d260cd36412e942f8a7b11da96d
charliedavidhoward <41549599+charliedavidhoward@users.noreply.github.com>
2021-05-20 08:05:39 +0100

    Create nonMutatingSort.js

---
+++ intermediate/nonMutatingSort.js
@@ -0,0 +1,10 @@
+// Use the sort method in the nonMutatingSort function to sort the elements of an array in ascending order. The function should return a new array, and not mutate the globalArray variable.
+
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  let newArr = arr.slice();
+  return newArr.sort(function(a, b) {
+    return a - b;
+  })
+}
+console.log(nonMutatingSort(globalArray));
