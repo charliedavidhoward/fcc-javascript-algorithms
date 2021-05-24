93a867fc93604aff4e46f13aab903192c1e676b1
charliedavidhoward <41549599+charliedavidhoward@users.noreply.github.com>
2021-05-24 22:05:09 +0100

    Create whereforeArtThou.js

---
+++ intermediate/whereforeArtThou.js
@@ -0,0 +1,18 @@
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+function whatIsInAName(collection, source) {
+  var sourceKeys = Object.keys(source);
+
+ return collection.filter(function(obj) {
+    for (var i = 0; i < sourceKeys.length; i++) {
+      if ( !obj.hasOwnProperty(sourceKeys[i]) ||
+        obj[sourceKeys[i]] !== source[sourceKeys[i]]
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
